1df6daaf8aa290cca3d6024a1015674331221e95
Marcus Elg <marcusaccounts@yahoo.se>
2021-06-11 09:21:21 +0200

    Fixed eating berry candle cake resulting in vanilla cake

--- src/main/java/moreberries/BlockBerryCake.java
+++ src/main/java/moreberries/BlockBerryCake.java
@@ -1,14 +1,15 @@
 package moreberries;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CakeBlock;
 import net.minecraft.block.CandleCakeBlock;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.event.GameEvent;
 
 import java.util.HashMap;
 
@@ -20,6 +21,25 @@ public class BlockBerryCake extends CakeBlock {
 		super(settings);
 		this.setDefaultState(this.getDefaultState().with(BITES, 0));
 		CANDLES_TO_CANDLE_CAKES = new HashMap<>();
+	}
+
+	public static ActionResult tryEat(WorldAccess worldAccess, BlockPos blockPos, BlockState blockState, PlayerEntity playerEntity) {
+		if (!playerEntity.canConsume(false)) {
+			return ActionResult.PASS;
+		} else {
+			playerEntity.incrementStat(Stats.EAT_CAKE_SLICE);
+			playerEntity.getHungerManager().add(2, 0.1F);
+			int i = (Integer)blockState.get(BITES);
+			worldAccess.emitGameEvent(playerEntity, GameEvent.EAT, blockPos);
+			if (i < 6) {
+				worldAccess.setBlockState(blockPos, (BlockState)blockState.with(BITES, i + 1), 3);
+			} else {
+				worldAccess.removeBlock(blockPos, false);
+				worldAccess.emitGameEvent(playerEntity, GameEvent.BLOCK_DESTROY, blockPos);
+			}
+
+			return ActionResult.SUCCESS;
+		}
 	}
 
 }
--- src/main/java/moreberries/BlockCandleBerryCake.java
+++ src/main/java/moreberries/BlockCandleBerryCake.java
@@ -2,12 +2,16 @@ package moreberries;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.CandleCakeBlock;
+import net.minecraft.block.*;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class BlockCandleBerryCake extends CandleCakeBlock {
     private BlockBerryCake cake;
@@ -21,5 +25,28 @@ public class BlockCandleBerryCake extends CandleCakeBlock {
     @Environment(EnvType.CLIENT)
     public ItemStack getPickStack(BlockView blockView, BlockPos blockPos, BlockState blockState) {
         return new ItemStack(cake);
+    }
+
+    public ActionResult onUse(BlockState blockState, World world, BlockPos blockPos, PlayerEntity playerEntity, Hand hand, BlockHitResult blockHitResult) {
+        ItemStack itemStack = playerEntity.getStackInHand(hand);
+        if (!itemStack.isOf(Items.FLINT_AND_STEEL) && !itemStack.isOf(Items.FIRE_CHARGE)) {
+            if (isHittingCandle(blockHitResult) && playerEntity.getStackInHand(hand).isEmpty() && (Boolean)blockState.get(LIT)) {
+                extinguish(playerEntity, blockState, world, blockPos);
+                return ActionResult.success(world.isClient);
+            } else {
+                ActionResult actionResult = ((BlockBerryCake)cake).tryEat(world, blockPos, cake.getDefaultState(), playerEntity);
+                if (actionResult.isAccepted()) {
+                    dropStacks(blockState, world, blockPos);
+                }
+
+                return actionResult;
+            }
+        } else {
+            return ActionResult.PASS;
+        }
+    }
+
+    private static boolean isHittingCandle(BlockHitResult blockHitResult) {
+        return blockHitResult.getPos().y - (double)blockHitResult.getBlockPos().getY() > 0.5D;
     }
 }
