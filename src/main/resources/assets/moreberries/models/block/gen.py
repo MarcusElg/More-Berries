0551d75f4aea0998457ecc7054147f4f62b7c7fe
Marcus Elg <marcusaccounts@yahoo.se>
2022-12-10 20:25:33 +0100

    Rename cake item textures

--- src/main/resources/assets/moreberries/models/block/gen.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for filename in os.listdir(os.getcwd()):
-    with open(os.path.join(os.getcwd(), filename), 'r') as f:  # open in readonly mode
-        new_text = f.read().replace("moreberries:block/", "TEMP").replace("moreberries:",
-                                                                          "moreberries:block/").replace("TEMP", "moreberries:block/")
-    with open(os.path.join(os.getcwd(), filename), 'w') as f:  # open in readonly mode
-        f.write(new_text)
